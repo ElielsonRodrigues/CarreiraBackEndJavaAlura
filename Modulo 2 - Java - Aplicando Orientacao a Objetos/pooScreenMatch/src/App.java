c54e114975e1382812ccdee2ca59e05ddbc626d1
Elielson Rodrigues <elielsonkwl@hotmail.com>
2025-12-06 21:35:56 -0300

    finalizando aula 01

--- Modulo 2 - Java - Aplicando Orientacao a Objetos/pooScreenMatch/src/App.java
+++ Modulo 2 - Java - Aplicando Orientacao a Objetos/pooScreenMatch/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Filme filme = new Filme();
+
+        filme.nome = "O hobbit";
+        filme.anoLancamento = 2021;
+        filme.duracaoEmMin = 180;
+
+        filme.exibeFichaTecnicia();
+
+        filme.avalia(10);
+        filme.avalia(5);
+        filme.avalia(7);
+
+        System.out.println(filme.somaAvaliacao);
+        System.out.println(filme.totalAvaliacao);
+        System.out.println(filme.pegaMedia());
+
     }
 }
--- Modulo 2 - Java - Aplicando Orientacao a Objetos/pooScreenMatch/src/Filme.java
+++ Modulo 2 - Java - Aplicando Orientacao a Objetos/pooScreenMatch/src/Filme.java
@@ -3,8 +3,24 @@ public class Filme {
     String nome;
     int anoLancamento;
     boolean incluindoPremium;
-    double avaliacao;
+    double somaAvaliacao;
     int totalAvaliacao;
     int duracaoEmMin;
-    
+
+    void exibeFichaTecnicia() {
+        System.out.println("""
+                nome: %s
+                ano: %d
+                """.formatted(nome, anoLancamento));
+    }
+
+    void avalia(double nota) {
+        somaAvaliacao += nota;
+        totalAvaliacao++;
+    }
+
+    double pegaMedia() {
+        return somaAvaliacao / totalAvaliacao;
+    }
+
 }
