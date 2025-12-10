4ebe4c30b50637598c214a0226064b2da57a076e
elielson.rodrigues <elielsonr.rodrigues@gmail.com>
2025-12-10 15:46:22 -0300

    Aula 01- consumindo api

---
+++ Modulo 4 - Java consumindo API, gravando arquivos e lidando com erros/consumindo-api/src/App.java
@@ -0,0 +1,29 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Informe o nome do filme:");
+        var nomefilme = sc.nextLine();
+
+        String url = "http://www.omdbapi.com/?t="+ nomefilme + "&apikey=5a10db1c";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+             .uri(URI.create(url))
+             .build();
+
+        HttpResponse<String> response = client
+            .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+
+    }
+}
