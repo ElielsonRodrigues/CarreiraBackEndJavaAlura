0c4d919b6e58f3ade2b358b4bdefb137113a6119
elielson.rodrigues <elielsonr.rodrigues@gmail.com>
2025-12-11 16:03:29 -0300

    Convertendo objetos json e record

--- Modulo 4 - Java consumindo API, gravando arquivos e lidando com erros/consumindo-api/src/App.java
+++ Modulo 4 - Java consumindo API, gravando arquivos e lidando com erros/consumindo-api/src/App.java
@@ -4,7 +4,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.alura.screenmatch.model.Titulo;
+import br.com.alura.screenmatch.record.TituloOmdbRecord;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -24,9 +29,37 @@ public class App {
 
         HttpResponse<String> response = client
             .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        //System.out.println(json);
 
         Gson gson = new Gson();
+
+        // Aplicando a converson utilizando a instancia do gson
+        Titulo titulo = gson.fromJson(json,Titulo.class);
+        //System.out.println(titulo.getNome());
+        
+        /*
+            Atenção! Por conta a serialização do json vim 'name' e minha classe
+            titulo o atributo ser 'nome' e retornado nome porque o gson não consegue
+            fazer a descerialização;
+            Para que se obtenha sucesso e necessario anotar os atributos com a classe
+            @SerializedName("Title"), porem em caso de consumo de apis de diferentes serviços
+            e levando em consideração boas preaticas necessario criar uma record(oficialmente no Java 16, mas disponível desde o Java 14)
+            como podemos ver abaixo
+        */
+        Gson gsonBuild = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                        .create();
+        
+        TituloOmdbRecord tituloOmdbRecord = gsonBuild.fromJson(json, TituloOmdbRecord.class);
+        System.out.println(tituloOmdbRecord);
+
+        Titulo t = new Titulo(tituloOmdbRecord);
+        System.out.println("Titulo Convertido: " + t);
+        
+
+          
 
 
         
---
+++ Modulo 4 - Java consumindo API, gravando arquivos e lidando com erros/consumindo-api/src/br/com/alura/screenmatch/record/TituloOmdbRecord.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.record;
+
+public record TituloOmdbRecord(String title, String year, String runtime) {
+
+    
+}
